4522dff7cb2d407084b21fb9b285a86184f42caa
SeungChul Shin <s.seungcheul@gmail.com>
2017-10-30 15:42:11 +0900

    spring security remember me

---
+++ security-rememberme/src/main/java/com/tistory/seungchul/RememberMe.java
@@ -0,0 +1,14 @@
+package com.tistory.seungchul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RememberMe {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(RememberMe.class, args);
+		
+		
+	}
+}
